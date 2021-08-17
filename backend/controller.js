3aedb4a910cb5a94a48b57726c2df9c80f242e0e
yetiasg <yetiasg@gmail.com>
2021-08-17 14:27:58 +0200

    Return original URL

--- backend/controller.js
+++ backend/controller.js
@@ -18,8 +18,9 @@ exports.shorten = async(req, res, next) => {
 exports.returnOriginalUrl = async(req, res, next) => {
   try{ 
     const newUrl = new URL(req.body.newUrl)
-    const urlValue = newUrl.hash.slice(1)
-    res.status(200).json({ urlValue });
+    const _id = newUrl.hash.slice(1)
+    const {originalURL} = await URLModel.findOne({_id});
+    res.status(200).json({ originalURL });
   }catch (error){
     next(error);
   }
